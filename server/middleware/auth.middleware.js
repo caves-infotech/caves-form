4f9d3d5cb6ad187da3de1ecaff9a06ef89ae47a8
Caves Infotech <cavesinfotech@gmail.com>
2024-09-12 18:26:25 +0530

    authorization header added in server middleware

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,8 +1,8 @@
 const {getUser} = require('../utils/auth');
 
 function authenticateUser(req, res, next){
-    // const token = req.headers['authorization']?.split(' ')[1];
-    const token = req.cookies?.token;
+    const token = req.headers['authorization']?.split(' ')[1];
+    // const token = req.cookies?.token;
     req.user = null;
     if(!token){
         next();
