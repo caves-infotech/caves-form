63d5824d67134f235b96fd1fc4823ea7e333efb1
cavesinfotech <cavesinfotech@gmail.com>
2024-10-27 16:29:49 +0530

    robots.js file added

---
+++ client/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+      rules: {
+        userAgent: '*',
+        allow: '/',
+      },
+      sitemap: 'https://udcprs.com/sitemap.xml',
+    }
+  }
