4f1cedacb85d83036cf1b35ccd33afc4d723d4ac
cavesinfotech <cavesinfotech@gmail.com>
2024-11-28 19:54:33 +0530

    navbar done

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -23,28 +23,34 @@ const Navbar = ({ isScrolled }) => {
       }`}
     >
       <div className="flex items-center justify-center">
-        <div className="flex items-center flex-wrap justify-center sm:p-2 sm:px-4 space-x-1 bg-white bg-opacity-10 rounded-2xl mx-2 ">
+        <div className="flex items-center flex-wrap justify-center sm:p-2  py-2  bg-white bg-opacity-10 rounded-3xl mx-2 ">
+          <div className="sm:p-2 my-1 sm:space-x-4 space-x-1">
           {row1Data.map((item, index) => (
             <Link
               href="/form"
               key={index}
               onClick={() => setState(item[1])}
-              className={` bg-yellow-500 px-2 sm:px-4 py-1 my-1 text-sm sm:text-xl font-semibold rounded-full hover:text-slate-700 cursor-pointer`}
+              className={` bg-yellow-500 px-2 sm:px-5 sm:p-2 py-1 my-1 text-sm sm:text-xl font-semibold rounded-xl hover:text-slate-700 cursor-pointer`}
             >
               {item[0]}
             </Link>
           ))}
-
+          </div>
+          
+          <div className="sm:p-2 my-1 sm:space-x-4 space-x-1">
           {row2Data.map((item, index) => (
             <Link
               href="/form"
               key={index}
               onClick={() => setState(item[1])}
-              className={` bg-yellow-500 px-2 sm:px-4 py-1 my-1 text-sm sm:text-xl font-semibold rounded-full hover:text-slate-700  cursor-pointer`}
+              className={` bg-yellow-500 px-2 sm:px-5 sm:p-2 py-1 my-1 text-sm sm:text-xl font-semibold rounded-xl hover:text-slate-700  cursor-pointer`}
             >
               {item[0]}
             </Link>
           ))}
+          </div>
+
+          
         </div>
       </div>
     </nav>
